fe46d0a117ae6a505b76de24f91fea805765b2a9
Sudipto <Sudiptosaha1111@gmail.com>
2025-06-12 12:00:21 +0530

    movie page

--- src/components/User/Home/MoviePage.js
+++ src/components/User/Home/MoviePage.js
@@ -42,29 +42,34 @@ const MoviePage = () => {
   return (
     <>
       <Usernavbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <div className="movie-page">
-        <div className="main-movie-name"><h2>Movies</h2></div>
+      <div className="pt-[90px] px-[10px] flex flex-col items-center mb-[10px] 
+      2xl:pt-[90px] 2xl:px-[20px] 2xl:flex 2xl:flex-col 2xl:items-center 2xl:mb-[30px]">
+        <div className="font-sans text-[#444441] mb-[10px]
+        2xl:font-sans 2xl:text-[#444441] 2xl:mb-[20px]"><h2>Movies</h2></div>
 
-        <div className="language-buttons">
-          <button className={`language-button ${selectedLanguages.length === 0 ? 'active' : ''}`} onClick={() => toggleLanguage("All")}>All</button>
+        <div className="flex flex-wrap justify-center gap-2 mb-6
+        2xl:flex 2xl:flex-wrap 2xl:justify-center 2xl:gap-2 2xl:mb-6">
+          <button className={`px-3 py-2 rounded-full      2xl:px-3 2xl:py-2 2xl:rounded-full ${selectedLanguages.length === 0 ? 'bg-orange-600 text-white' : 'bg-gray-200 text-gray-700'}`} onClick={() => toggleLanguage("All")}>All</button>
           {["Hindi", "English", "Marathi", "Gujarati", "Tamil", "Telugu", "Bengali"].map(lang => (
-            <button key={lang} className={`language-button ${selectedLanguages.includes(lang) ? 'active' : ''}`} onClick={() => toggleLanguage(lang)}>{lang}</button>
+            <button key={lang} className={`px-3 py-2 rounded-full    2xl:px-3 2xl:py-2 2xl:rounded-full ${selectedLanguages.includes(lang) ? 'bg-orange-600 text-white' : 'bg-gray-200 text-gray-700'}`} onClick={() => toggleLanguage(lang)}>{lang}</button>
           ))}
         </div>
 
-        <div className="movie-cards-container">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[35px] 2xl: gap-[70px]">
           {filteredMovies.map(movie => (
             <Link
               to={`/moviedetails/${movie._id}`}
               key={movie._id}
               state={movie}
-              className="movie-card-link"
+              className="text-current no-underline"
               onClick={() => localStorage.setItem("id", movie._id)}
             >
-              <div className="movie-card">
-                <img src={movie.image} alt={movie.movieName} className="movie-poster" />
-                <h3 className="movie-title">{movie.movieName}</h3>
-                <p className="movie-language">{movie.movieLanguage}</p>
+              <div className="w-[150px] h-[280px] rounded-[8px] mb-[-30px] overflow-hidden cursor-pointer text-center flex flex-col justify-start transition-transform transition-shadow duration-300 ease hover:-translate-y-[5px] 
+              2xl:w-[220px] 2xl:h-[425px] ">
+                <img src={movie.image} alt={movie.movieName} className="w-full h-[200px] object-cover
+                                                                        2xl:w-full 2xl:h-[320px]" />
+                <h3 className="mt-[5px] mb-[10px] text-[1rem] font-bold text-left hover:text-black-900 ">{movie.movieName}</h3>
+                
               </div>
             </Link>
           ))}
