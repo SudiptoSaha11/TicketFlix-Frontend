64a887d2cb0109d2a1700de924e57b93f4b8a638
Sudipto <Sudiptosaha1111@gmail.com>
2025-06-18 12:10:21 +0530

    logo

--- src/components/User/Home/Ticketbooking.js
+++ src/components/User/Home/Ticketbooking.js
@@ -151,7 +151,7 @@ const Ticketbooking = () => {
       if (result.error) {
         console.error(result.error.message);
       } else {
-        navigate('https://ticketflix-backend.onrender.com/success');
+        navigate('https://ticketflix-official.netlify.app/success');
       }
     } catch (error) {
       console.error('Error during payment:', error);
