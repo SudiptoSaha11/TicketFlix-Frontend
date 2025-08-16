b6524978977d5472dd34de305a5bdd86f4ec2bab
Sudipto <Sudiptosaha1111@gmail.com>
2025-08-16 14:01:17 +0530

    login auth

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-//   baseURL: 'https://ticketflix-official.netlify.app',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://ticketflix-official.netlify.app',
+//   baseURL: 'http://localhost:5000',
 });
 
 export default api;
