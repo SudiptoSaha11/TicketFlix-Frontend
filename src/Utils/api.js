dc1942ef3b7100695084f99b3230f1894dad49ad
Sudipto <Sudiptosaha1111@gmail.com>
2025-10-26 18:02:46 +0530

    last commit

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // or use an env variable (explained below)
-  // You can also set default headers here if needed
+  // baseURL: 'http://localhost:5000' 
+  baseURL: 'https://ticketflix-backend.onrender.com' 
 });
 
 export default api;
